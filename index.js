c33292d6aff8f4f80e76c2426ea195175e5c962d
SergeyDedikov <serded59@yandex.ru>
2023-03-17 00:50:25 +0500

    feat: create and insert cards function

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const list = document.getElementById('repos');
 */
 const BASE_URL = 'https://api.github.com';
 const PATH_NAME = '/search/repositories';
-const HEADERS_ACCEPT = 'application/vnd.github+json';
+const HEADERS = {
+  'Accept': 'application/vnd.github+json'
+};
 
 const url = new URL(PATH_NAME, BASE_URL);
 
@@ -22,23 +24,69 @@ function createSearchString(url, query) {
 
 let repositories = [];
 
+// создаём карточку репозитория
+function createCard(data) {
+
+  let card = document.createElement('li');
+  card.className = 'repo flex';
+
+  card.insertAdjacentHTML(
+    'afterbegin',
+    `<div>
+      <img
+        class="repo__avatar"
+        alt="Аватар владельца"
+        src=${data.owner.avatar_url}>
+    </div>
+    <div class="flex column">
+      <p>Владелец: <b>${data.owner.login}</b></p>
+      <nav>
+        <a
+          class="repo__name"
+          href=${data.html_url}
+          target="_blank">
+          ${data.name}
+        </a>
+      </nav>
+      <p>&starf; ${(data.stargazers_count) ? data.stargazers_count : '0'}</p>
+      <p>${(data.description) ? data.description : ''}</p>
+      <p>Последние изменения: <b>${(data.updated_at).slice(0, 10)}</b></p>
+    </div>`
+  );
+
+  return card;
+}
+
+// вставляем карточку в разметку
+function insertCard(container, card) {
+  container.append(card);
+}
+
+// очищаем разметку
+function clearList(container) {
+  if (!container.children.length == 0) {
+    container.innerHTML = '';
+  }
+}
+
 // отправка запроса
-function sendQuery(e) {
+function formSubmit(e) {
 
   e.preventDefault();
 
   // тут валидация
 
+  // очищаем предыдущий результат
+  clearList(list);
+
   // получаем строку из поля ввода и создаём url поиска
   let searchUrl = createSearchString(url, this.query.value)
-  
+
   // отправляем запрос на сервер
   fetch(searchUrl,
     {
       method: "GET",
-      headers: {
-        'Accept': HEADERS_ACCEPT
-      },
+      headers: HEADERS,
     })
     .then(async (res) => {
       if (res.ok) {
@@ -54,6 +102,12 @@ function sendQuery(e) {
       // берём первые 10
       repositories = data.items.slice(0, 10);
       console.log(repositories);
+      // создаём карточки и вставляем в разметку
+      repositories.forEach((item) => insertCard(list, createCard(item)));
+      // снимаем фокус с инпута
+      this.query.blur();
+      // очищаем поле ввода
+      this.reset();
     })
     .catch(err => console.log(err));
 
@@ -62,4 +116,4 @@ function sendQuery(e) {
 /**
  * Event Listeners
 */
-form.addEventListener('submit', sendQuery);
+form.addEventListener('submit', formSubmit);
