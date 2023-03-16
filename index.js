230df891bfe20ddcce14bf2832f33987c1c3ffa6
SergeyDedikov <serded59@yandex.ru>
2023-03-16 21:45:46 +0500

    feat: function sendQuery

--- index.js
+++ index.js
@@ -20,4 +20,46 @@ function createSearchString(url, query) {
   return url.href + '+in:name'; // ищем по именам
 }
 
-console.log(createSearchString(url, 'yopta'));
+let repositories = [];
+
+// отправка запроса
+function sendQuery(e) {
+
+  e.preventDefault();
+
+  // тут валидация
+
+  // получаем строку из поля ввода и создаём url поиска
+  let searchUrl = createSearchString(url, this.query.value)
+  
+  // отправляем запрос на сервер
+  fetch(searchUrl,
+    {
+      method: "GET",
+      headers: {
+        'Accept': HEADERS_ACCEPT
+      },
+    })
+    .then(async (res) => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        const data = await res.json();
+        const message = data.message || "Что-то пошло не так!";
+        return await Promise.reject(message);
+      }
+    })
+    .then(data => {
+      // получаем данные с сервера
+      // берём первые 10
+      repositories = data.items.slice(0, 10);
+      console.log(repositories);
+    })
+    .catch(err => console.log(err));
+
+}
+
+/**
+ * Event Listeners
+*/
+form.addEventListener('submit', sendQuery);
